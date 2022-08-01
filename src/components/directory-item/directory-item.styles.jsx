e642c8d2127f84fe4ca8f6712b8d81e2d7327eba
Stefan Forslin <stefan.forslin@iths.se>
2022-08-01 15:15:17 +0200

    fixed some css in styled components

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 export const BackgroundImage = styled.div`
   width: 100%;
   height: 100%;
@@ -6,6 +7,7 @@ export const BackgroundImage = styled.div`
   background-position: center;
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
+
 export const Body = styled.div`
   height: 90px;
   padding: 0 25px;
@@ -29,6 +31,7 @@ export const Body = styled.div`
     font-size: 16px;
   }
 `;
+
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
   height: 240px;
@@ -39,20 +42,20 @@ export const DirectoryItemContainer = styled.div`
   border: 1px solid black;
   margin: 0 7.5px 15px;
   overflow: hidden;
-  &:hover {
-    cursor: pointer;
-    & ${BackgroundImage} {
-      transform: scale(1.1);
-      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-    }
-    & ${Body} {
-      opacity: 0.9;
-    }
-  }
   &:first-child {
     margin-right: 7.5px;
   }
   &:last-child {
     margin-left: 7.5px;
+  }
+  &:hover {
+    cursor: pointer;
+    ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+    ${Body} {
+      opacity: 0.9;
+    }
   }
 `;
