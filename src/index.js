0493ff3bd0fcfb009004e5fa3f12c91260662768
Stefan Forslin <stefan.forslin@iths.se>
2022-09-19 16:06:33 +0200

    added categories

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import './index.scss';
 import App from './App';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,17 +12,11 @@ root.render(
   <React.StrictMode>
 	<Provider store={store}>
 	<BrowserRouter>
-	<CategoriesProvider>
 	<CartProvider>
     <App />
 	</CartProvider>
-	</CategoriesProvider>
 	</BrowserRouter>
 	</Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
